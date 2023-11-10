126c94e2ef3ba95c134c45c754b1bb795687978a
Peter Heneback <heneback@amazon.co.uk>
2023-11-10 12:54:42 +0000

    Added cdk-nag

--- cdk/bin/pipeline.ts
+++ cdk/bin/pipeline.ts
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 import { App } from 'aws-cdk-lib';
 import { CdkPipelineStack } from '../lib/cdk-pipeline-stack';
+import { AwsSolutionsChecks } from 'cdk-nag';
+import { Aspects } from 'aws-cdk-lib';
 
 const app = new App();
+// Add the cdk-nag AwsSolutions Pack with extra verbose logging enabled.
+Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }))
 
 new CdkPipelineStack(app, 'CdkPipelineStack', {
   env: { 
